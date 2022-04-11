ea946741f986a35316b5a3aa81b3563e5af30587
seungho <50465901+csh9066@users.noreply.github.com>
2022-04-12 02:02:08 +0900

    인증 프로세스 Form 인증 구현 실습 시작

---
+++ core-spring-security/src/test/java/hello/corespringsecurity/CoreSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.corespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
